91143b1759f71a8618595f0005c882a97804f1df
babyleak <dan4ykplay@gmail.com>
2023-07-09 17:36:57 +0500

    new files has been added

--- app/src/main/java/com/itis/taskmanager/TimeFragment.kt
+++ app/src/main/java/com/itis/taskmanager/TimeFragment.kt
@@ -1,23 +1,40 @@
-package com.example.taskmanager
+package com.itis.taskmanager
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
-import android.view.View
-import com.itis.taskmanager.R
+import androidx.databinding.DataBindingUtil.setContentView
 import com.itis.taskmanager.databinding.FragmentTimeBinding
 
 
 class TimeFragment : Fragment(R.layout.fragment_time) {
-
-    private var binding: FragmentTimeBinding? = null
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding = FragmentTimeBinding.bind(view)
+    private lateinit var binding: FragmentTimeBinding
+    private var timer: CountDownTimer? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentTimeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            bStart.setOnClickListener {
+                startCountDownTimer(20000)
+            }
+        }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        binding = null
+    private fun startCountDownTimer(timeMillis: Long){
+        timer?.cancel()
+        timer = object : CountDownTimer(timeMillis, 1){
+            override fun onTick(timeM: Long) {
+                binding.textView3.text = timeM.toString()
+            }
+
+            override fun onFinish() {
+                binding.textView3.text = "Finish"
+            }
+
+        }.start()
+    }
+    private fun setContentView(root: FrameLayout) {
     }
 }
